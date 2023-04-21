127d4dbd21bfbb737f07dc7394a36dbfcdc172ed
Sander Dyngeland <83211464+Sanderdyngis@users.noreply.github.com>
2023-04-21 11:20:31 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -15,10 +15,10 @@ application = Flask(__name__)
 # application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
 ### Code computer
-# application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
-# DBVAR="postgresql://username:password@host:port/database"
-# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-# application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+DBVAR="postgres://zoheeckhkpxbcv:e7feb75228a074fdd37e60d7c149df58f4ba3e011adc2459e83841483a7a3306@ec2-34-250-252-161.eu-west-1.compute.amazonaws.com:5432/df844lun2nfesb"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
